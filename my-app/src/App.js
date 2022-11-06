b60dbc38a594a59c2df92b2043de6947fed9af73
Adele Smolansky <as2953@cornell.edu>
2022-11-06 11:09:39 -0500

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
+
+import Navbar from './components/Navbar/Navbar';
+import About from './pages/About/About';
 import AI_Learners from './pages/AI-Learners';
 import Research from './pages/Research';
 
+import './App.css';
+
 function App() {
-return (
-	<Router>
-    <Navbar />
-    
-    <Routes>
-      <Route path='/' exact component={About} />
-      <Route path='/about' component={About} />
-      <Route path='/ai-learners' component={AI_Learners} />
-      <Route path='/research' component={Research} />
-    </Routes>
-	</Router>
-);
+  return (
+    <Router>
+      <Navbar />
+      
+      <Routes>
+        <Route exact path='/' component={About} />
+        <Route path='/about' component={About} />
+        <Route path='/ai-learners' component={AI_Learners} />
+        <Route path='/research' component={Research} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ my-app/src/pages/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>Adele Smolansky</h1>
+			<p>Adele Smolansky is a senior studying computer science at Cornell University</p>
+		</div>
+	)
+};
+
+export default About;
