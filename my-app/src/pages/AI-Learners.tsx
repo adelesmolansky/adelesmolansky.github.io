5411add12a2c6a83edef4eb739ae3a34e5ae464a
Adele Smolansky <as2953@cornell.edu>
2022-11-06 15:30:19 -0500

    small change

--- my-app/src/pages/AI-Learners.tsx
+++ my-app/src/pages/AI-Learners.tsx
@@ -2,15 +2,8 @@ import React from 'react';
 
 const AI_Learners = () => {
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>AI-Learners</h1>
+	<div>
+		<h1>AI-Learners</h1>
 	</div>
 );
 };
