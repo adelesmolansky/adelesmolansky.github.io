e4e44847fe3bff6dc0cef9ce1593864b0493adad
Adele Smolansky <as2953@cornell.edu>
2022-11-07 02:04:01 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import About from "./pages/About/About";
@@ -11,7 +11,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
 
       <Routes>
@@ -21,7 +21,7 @@ function App() {
         <Route path="/research" element={<Research />} />
         <Route path="/news" element={<News />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
